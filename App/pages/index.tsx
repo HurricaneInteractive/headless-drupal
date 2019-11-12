90b3fc848fb0c215d836f11ded1620fd8a51c56e
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-12 20:32:19 +1100

    Modified home page to pull articles

--- App/pages/index.tsx
+++ App/pages/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const Home = () => (
-  <div>
-    Hello World
-  </div>
-)
+import { Article } from "../DataModel"
+import ArticleData from '../DataModel/Article/ArticleData'
+import { NextPage } from 'next'
+import Link from "next/link"
+
+interface HomeProps {
+  articles: ArticleData[]
+}
+
+const Home: NextPage<HomeProps> = ({ articles }) => {
+  console.log(articles)
+  return (
+    <div>
+      {
+        articles.map(({ id, attributes: { title } }: ArticleData) => (
+          <Link key={id} passHref href={`/articles/${id}`}>
+            <a>{title}</a>
+          </Link>
+        ))
+      }
+    </div>
+  )
+}
+
+Home.getInitialProps = async () => {
+  const articles: ArticleData[] = await new Article().getAll();
+
+  return {
+    articles
+  }
+}
 
 export default Home
