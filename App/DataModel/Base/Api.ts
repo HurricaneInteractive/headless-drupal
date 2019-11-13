4c32aa69ef9904b808713da7faeabb58d990918d
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-13 15:49:38 +1100

    Add cors everywhere url if in browser

--- App/DataModel/Base/Api.ts
+++ App/DataModel/Base/Api.ts
@@ -1,10 +1,15 @@
 import fetch from 'isomorphic-unfetch'
+import inBrowserEnv from "in-browser-env"
 
 class DrupalAPI {
   private host: string = "http://dahcrosnza3qm.devcloud.acquia-sites.com/jsonapi"
 
   private performFetch = async (url: string) => {
-    const res = await fetch(`${url}`)
+    const res = await fetch(
+      inBrowserEnv()
+        ? `https://cors-anywhere.herokuapp.com/${url}`
+        : url
+    )
     const json = await res.json();
 
     return json
